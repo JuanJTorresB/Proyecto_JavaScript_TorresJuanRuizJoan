3f5bfc569dd84ad795019807d93d4dd716700a4d
JuanJTorresB <juanjtorresbecerra@gmail.com>
2024-08-14 14:36:10 -0500

    feat: :sparkles: Se creo la logica de la peticion de los personajes

--- script.js
+++ script.js
@@ -18,9 +18,20 @@ const petición = async (url, opciones)=>{
 
 const url = "https://swapi.py4e.com/api/"
 
+let pagina = 1
+
+// containerPersonajes
+
+const containerPersonajes = document.getElementById("containerPersonajes")
+
 async function ejecutarPeticiónPersonajes (){
-    const resultadoObtenido = await petición(url, opciones)
-    console.log(resultadoObtenido)
+    const resultadoObtenido = await petición(`${url}/people/?page=${pagina}`, opciones)
+    resultadoObtenidoJSON = await resultadoObtenido.JSON
+    console.log(resultadoObtenido.results)
+    for (let personaje of resultadoObtenido.results){
+      let personajeCard = crearElementoPersonaje(personaje)
+      containerPersonajes.appendChild(personajeCard)
+    }
 }
 
 ejecutarPeticiónPersonajes()
@@ -29,19 +40,21 @@ ejecutarPeticiónPersonajes()
 
 const crearElementoPersonaje = (dataPersonaje)=>{
     let elementoPersonaje = document.createElement("div")
-    elementoPersonaje.innerHTML = `<div class="bg-white rounded-lg border p-4">
-    <img src="https://placehold.co/300x200/d1d4ff/352cb5.png" alt="Placeholder Image"
-      class="w-full h-48 rounded-md object-cover">
-    <div class="px-1 py-4">
-      <div class="font-bold text-xl mb-2">Blog Title</div>
-      <p class="text-gray-700 text-base">
-        This is a simple blog card example using Tailwind CSS. You can replace this text with your own blog
-        content.
-      </p>
-    </div>
-    <div class="px-1 py-4">
-      <a href="#" class="text-blue-500 hover:underline">Read More</a>
-    </div>
+    elementoPersonaje.classList.add("bg-white")
+    elementoPersonaje.classList.add("rounded-lg")
+    elementoPersonaje.classList.add("border")
+    elementoPersonaje.classList.add("p-4")
+    elementoPersonaje.innerHTML = `<div class="px-1 py-4">
+    <div class="font-bold text-xl mb-2">${dataPersonaje.name}</div>
+    <ul class="text-gray-700 text-base">
+      <li><b>Gender:</b> ${dataPersonaje.gender}</li>
+      <li><b>Height:</b> ${dataPersonaje.height} cm</li>
+      <li><b>Hair Color:</b> ${dataPersonaje.hair_color}</li>
+      <li><b>Eye Color:</b> ${dataPersonaje.eye_color} </li>
+    </ul>
+  </div>
+  <div class="px-1 py-4">
+    <a href="#" class="text-blue-500 hover:underline">Read More</a>
   </div>`
   console.log(elementoPersonaje)
   return elementoPersonaje
@@ -60,5 +73,3 @@ const crearElementoPersonaje = (dataPersonaje)=>{
             </div>
           </div> */
 }
-
-crearElementoPersonaje()
